04deabe9dd35ec85dc9beb32cb3d5615d735f1a6
Devyn Cairns <devyn.cairns@gmail.com>
2014-05-19 04:17:31 -0700

    Updated paws_rs program to advance the script as an Execution repeatedly with a static symbol

--- src/bin/paws_rs.rs
+++ src/bin/paws_rs.rs
@@ -2,24 +2,78 @@ extern crate paws;
 
 use std::io;
 use std::os;
+use std::any::AnyMutRefExt;
 
 use paws::cpaws;
-use paws::machine::Machine;
+use paws::machine::{Machine, Combination};
+use paws::object::{Object, ObjectRef};
+use paws::object::execution::Execution;
 
 fn main() {
   let input = io::stdin().read_to_str()
                 .ok().expect("reading from stdin failed");
 
+  let mut stdout = io::stdout();
+
   match cpaws::parse_nodes(input, "<stdin>") {
     Ok(nodes) => {
       let mut machine = Machine::new();
 
+      let test_symbol = ObjectRef::new(~machine.symbol("test"));
+
       let script = cpaws::build_script(&mut machine, nodes);
 
-      script.fmt_paws(&mut io::stdout(), &machine)
-        .ok().expect("fmt_paws did not succeed!");
+      let execution_ref =
+        ObjectRef::new(~Execution::new(script));
 
-      io::print("\n");
+      println!("Advancing script as Execution with \"test\" symbol.");
+
+      loop {
+        stdout.write_str("\n    ").unwrap();
+
+        let mut maybe_combination: Option<Combination>;
+
+        {
+          let mut execution_ref_borrow = execution_ref.borrow_mut();
+
+          let execution: &mut Execution =
+            execution_ref_borrow.as_mut_any().as_mut().unwrap();
+
+          execution.fmt_paws(&mut stdout, &machine)
+            .ok().expect("fmt_paws did not succeed!");
+
+          stdout.write_str("\n\n").unwrap();
+
+          maybe_combination =
+            execution.advance(execution_ref.clone(), test_symbol.clone());
+        }
+
+        match maybe_combination {
+          Some(combination) => {
+            stdout.write_str("(").unwrap();
+
+            match combination.subject {
+              None => stdout.write_str("#<locals>").unwrap(),
+              Some(ref subject_ref) => {
+                let subject_borrow = subject_ref.borrow();
+
+                subject_borrow.fmt_paws(&mut stdout, &machine)
+                  .ok().expect("fmt_paws did not succeed!");
+              }
+            }
+
+            stdout.write_str(" <- ").unwrap();
+
+            let message_borrow = combination.message.borrow();
+
+            message_borrow.fmt_paws(&mut stdout, &machine)
+              .ok().expect("fmt_paws did not succeed!");
+
+            stdout.write_str(")").unwrap();
+          },
+          None => break
+        }
+      }
     }
 
     Err(message) => {
