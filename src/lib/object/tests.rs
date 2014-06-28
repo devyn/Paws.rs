5fdab3553028f3fd5ff77849bb9a9a3ee644f517
Devyn Cairns <devyn.cairns@gmail.com>
2014-06-28 01:15:02 -0700

    Lookup receiver tests implemented

--- src/lib/object/tests.rs
+++ src/lib/object/tests.rs
@@ -2,6 +2,8 @@ use object::*;
 use object::empty::Empty;
 use object::symbol::Symbol;
 
+use machine::*;
+
 use sync::Arc;
 
 #[test]
@@ -101,4 +103,150 @@ fn mixed_refs_eq_as_symbol_is_false() {
 
   assert!(!empty_ref.eq_as_symbol(&symbol_ref));
   assert!(!symbol_ref.eq_as_symbol(&empty_ref));
+}
+
+struct LookupReceiverTestEnv {
+  machine:     Machine,
+
+  target_ref:  ObjectRef,
+  caller_ref:  ObjectRef,
+
+  obj_key_ref: ObjectRef,
+  obj_val_ref: ObjectRef,
+
+  sym_key_sym: Arc<~str>,
+  sym_key_ref: ObjectRef,
+  sym_val_ref: ObjectRef
+}
+
+// Maybe this should be a helper inside the library? Seems pretty useful.
+fn make_pair(key: ObjectRef, value: ObjectRef) -> ObjectRef {
+  let pair_ref = ObjectRef::new(~Empty::new());
+
+  {
+    let mut pair = pair_ref.lock().try_cast::<Empty>().unwrap();
+
+    pair.meta_mut().members.push(None);
+    pair.meta_mut().members.push(Some(Relationship::new(key)));
+    pair.meta_mut().members.push(Some(Relationship::new(value)));
+  }
+
+  pair_ref
+}
+
+fn setup_lookup_receiver_test() -> LookupReceiverTestEnv {
+  let mut machine = Machine::new();
+
+  let target_ref  = ObjectRef::new(~Empty::new());
+  let caller_ref  = ObjectRef::new(~Empty::new());
+
+  let obj_key_ref = ObjectRef::new(~Empty::new());
+  let obj_val_ref = ObjectRef::new(~Empty::new());
+
+  let sym_key_ref = machine.symbol("foo");
+  let sym_key_sym = sym_key_ref.symbol_ref().unwrap().clone();
+  let sym_val_ref = machine.symbol("bar");
+
+  {
+    let mut target = target_ref.lock().try_cast::<Empty>().unwrap();
+
+    target.meta_mut().members.push(None);
+
+    target.meta_mut().members.push(Some(Relationship::new(
+      make_pair(obj_key_ref.clone(), obj_val_ref.clone()))));
+
+    target.meta_mut().members.push(Some(Relationship::new(
+      make_pair(sym_key_ref.clone(), sym_val_ref.clone()))));
+  }
+
+  LookupReceiverTestEnv {
+    machine:     machine,
+
+    target_ref:  target_ref,
+    caller_ref:  caller_ref,
+
+    obj_key_ref: obj_key_ref,
+    obj_val_ref: obj_val_ref,
+
+    sym_key_ref: sym_key_ref,
+    sym_key_sym: sym_key_sym,
+    sym_val_ref: sym_val_ref
+  }
+}
+
+#[test]
+fn lookup_receiver_hit_object_key() {
+  let mut env = setup_lookup_receiver_test();
+
+  let reaction = lookup_receiver(&mut env.machine, Params {
+    caller:  env.caller_ref.clone(),
+    subject: env.target_ref.clone(),
+    message: env.obj_key_ref.clone()
+  });
+
+  match reaction {
+    React(Stage(ref stage_params)) => {
+      assert!(&stage_params.execution == &env.caller_ref);
+      assert!(&stage_params.response  == &env.obj_val_ref);
+    },
+
+    _ => fail!("unexpected reaction!")
+  }
+}
+
+#[test]
+fn lookup_receiver_hit_symbol_key() {
+  let mut env = setup_lookup_receiver_test();
+
+  let reaction = lookup_receiver(&mut env.machine, Params {
+    caller:  env.caller_ref.clone(),
+    subject: env.target_ref.clone(),
+    message: ObjectRef::new_symbol(
+               ~Symbol::new(env.sym_key_sym.clone()))
+  });
+
+  match reaction {
+    React(Stage(ref stage_params)) => {
+      assert!(&stage_params.execution == &env.caller_ref);
+      assert!(&stage_params.response  == &env.sym_val_ref);
+    },
+
+    _ => fail!("unexpected reaction! expected React(Stage(...))")
+  }
+}
+
+#[test]
+fn lookup_receiver_miss_object_key() {
+  let mut env = setup_lookup_receiver_test();
+
+  let reaction = lookup_receiver(&mut env.machine, Params {
+    caller:  env.caller_ref.clone(),
+    subject: env.target_ref.clone(),
+    message: ObjectRef::new(~Empty::new())
+  });
+
+  match reaction {
+    Yield => (),
+
+    _ => fail!("unexpected reaction! expected Yield")
+  }
+}
+
+#[test]
+fn lookup_receiver_miss_symbol_key() {
+  let mut env = setup_lookup_receiver_test();
+
+  let bar_sym_ref = env.machine.symbol("bar");
+
+  let reaction = lookup_receiver(&mut env.machine, Params {
+    caller:  env.caller_ref.clone(),
+    subject: env.target_ref.clone(),
+    message: bar_sym_ref
+  });
+
+  match reaction {
+    Yield => (),
+
+    _ => fail!("unexpected reaction! expected Yield")
+  }
 }
