3187c0e338b31cbb9135058c5d5947b7593661e8
Devyn Cairns <devyn.cairns@gmail.com>
2014-07-03 02:46:23 -0700

    Call pattern helper <3

--- src/lib/object/alien/mod.rs
+++ src/lib/object/alien/mod.rs
@@ -6,6 +6,7 @@ use machine::Machine;
 
 use std::any::*;
 use std::io::IoResult;
+use std::mem::replace;
 
 #[cfg(test)]
 mod tests;
@@ -33,13 +34,28 @@ pub struct Alien {
 }
 
 impl Alien {
-  /// Construct an Alien around a given Routine.
+  /// Construct an Alien around a given `Routine`.
   pub fn new(routine: Routine, data: Box<Any+'static+Send+Share>) -> Alien {
     Alien {
       routine: routine,
       data:    data,
       meta:    Meta::new()
     }
+  }
+
+  /// Construct a call-pattern Alien which calls the given `CallPatternRoutine`
+  /// once `n_args` arguments have been accepted.
+  pub fn new_call_pattern(routine: CallPatternRoutine, n_args: uint) -> Alien {
+    let call_pattern_data = box CallPatternData {
+      caller:    None,
+      args:      Vec::with_capacity(n_args),
+      complete:  false,
+      remaining: n_args,
+      routine:   routine
+    };
+
+    Alien::new(call_pattern_alien_routine,
+               call_pattern_data as Box<Any+'static+Send+Share>)
   }
 }
 
@@ -63,7 +79,102 @@ impl Object for Alien {
 
 /// A function that implements the logic behind an Alien.
 pub type Routine = fn <'a>(
-                       alien: TypedRefGuard<'a, Alien>,
-                       machine: &Machine,
+                       alien:    TypedRefGuard<'a, Alien>,
+                       machine:  &Machine,
                        response: ObjectRef)
                        -> Reaction;
+
+/// A function that implements a "call-pattern" style Alien.
+///
+/// This type of alien first accepts a 'caller', then (optionally) repeatedly
+/// responds to that caller with itself to accumulate more arguments until it
+/// reaches a pre-defined number, at which point the actual Alien logic is run.
+///
+/// After all of the arguments and caller have been accepted and a response has
+/// been sent from the routine, the Alien no longer responds.
+///
+/// # Example
+///
+/// cPaws code (where `alien` is a call-pattern alien that takes a caller and 2
+/// args):
+///
+///     alien() hello world
+///
+/// Assume this returns `hi`. Timeline:
+///
+///     alien <- ()    ... {caller = ()}        ... caller <- alien
+///     alien <- hello ... {args   push(hello)} ... caller <- alien
+///     alien <- world ... {args   push(world)} ...
+///
+///       call_pattern_routine(machine, caller = (), args = [hello, world])
+///         -> React(caller, hi) ...
+///
+///     caller <- hi
+pub type CallPatternRoutine = fn<'a>(
+                                 machine: &Machine,
+                                 caller:  ObjectRef,
+                                 args:    &[ObjectRef])
+                                 -> Reaction;
+
+/// Internal state for call pattern wrapper.
+struct CallPatternData {
+  caller:    Option<ObjectRef>,
+  args:      Vec<ObjectRef>,
+  complete:  bool,
+  remaining: uint,
+  routine:   CallPatternRoutine
+}
+
+/// Function that performs call pattern wrapper.
+fn call_pattern_alien_routine<'a>(
+                              mut alien: TypedRefGuard<'a, Alien>,
+                              machine:   &Machine,
+                              response:  ObjectRef)
+                              -> Reaction {
+
+  let (caller, routine, args) = {
+    // Do everything we need to do to data in here, so we can drop alien.
+    let data = alien.data.as_mut::<CallPatternData>().unwrap();
+
+    // Don't do anything if we're complete.
+    if data.complete { return Yield }
+
+    match data.caller {
+      Some(_) => {
+        data.args.push(response);
+        data.remaining -= 1;
+      },
+      None => {
+        data.caller = Some(response.clone());
+      }
+    }
+
+    if data.remaining == 0 {
+      let routine = data.routine;
+
+      // Cheap way to deallocate all of the expensive stuff in data.
+      let final = replace(data, CallPatternData {
+        caller:    None,
+        args:      Vec::new(),
+        complete:  true,
+        remaining: 0,
+        routine:   routine
+      });
+
+      (final.caller.unwrap(), final.routine, Some(final.args))
+    } else {
+      (data.caller.get_ref().clone(), data.routine, None)
+    }
+  };
+
+  match args {
+    Some(args) => {
+      // We have args, so we must be done.
+      drop(alien);
+      routine(machine, caller, args.as_slice())
+    },
+    None =>
+      // Need more args!
+      React(caller, alien.unlock().clone())
+  }
+}
--- src/lib/object/alien/tests.rs
+++ src/lib/object/alien/tests.rs
@@ -1,6 +1,7 @@
 use machine::*;
 use object::*;
 use object::alien::Alien;
+use object::empty::Empty;
 
 use std::any::*;
 
@@ -19,10 +20,10 @@ mod simple {
 
   #[allow(unused_variable)]
   fn routine<'a>(
-                 mut alien: TypedRefGuard<'a, Alien>,
-                 machine:   &Machine,
-                 response:  ObjectRef)
-                 -> Reaction {
+             mut alien: TypedRefGuard<'a, Alien>,
+             machine:   &Machine,
+             response:  ObjectRef)
+             -> Reaction {
 
     match response.lock().try_cast::<Symbol>() {
       Ok(symbol) =>
@@ -61,4 +62,57 @@ fn simple_alien() {
 
   assert!(alien.deref().data.as_ref::<String>()
           .unwrap().as_slice() == "Hello, world!");
+}
+
+#[test]
+fn call_pattern_alien() {
+  let machine = Machine::new();
+
+  // Returns concatenation of arguments if all three are symbols, otherwise
+  // fails. (normally you'd just not want to return)
+  fn routine<'a>(
+             machine: &Machine,
+             caller:  ObjectRef,
+             args:    &[ObjectRef])
+             -> Reaction {
+
+    let cat_str = args.iter().fold(String::new(), |s, o|
+      s.append(o.symbol_ref().expect("expected Symbol")
+                                            .as_slice()));
+
+    React(caller, machine.symbol(cat_str.as_slice()))
+  }
+
+  let caller_ref = ObjectRef::new(box Empty::new());
+
+  let alien_ref = ObjectRef::new(box
+                    Alien::new_call_pattern(routine, 3));
+
+  let assert_caller_and_alien = |send| {
+    let alien = alien_ref.lock().try_cast::<Alien>().ok().unwrap();
+
+    match (alien.routine)(alien, &machine, send) {
+      React(execution, response) => {
+        assert!(&execution == &caller_ref);
+        assert!(&response  == &alien_ref);
+      },
+      _ => fail!("Unexpected reaction!")
+    }
+  };
+
+  assert_caller_and_alien(caller_ref.clone());
+  assert_caller_and_alien(machine.symbol("a"));
+  assert_caller_and_alien(machine.symbol("b"));
+
+  {
+    let alien = alien_ref.lock().try_cast::<Alien>().ok().unwrap();
+
+    match (alien.routine)(alien, &machine, machine.symbol("c")) {
+      React(execution, response) => {
+        assert!(&execution == &caller_ref);
+        assert!( response.symbol_ref().unwrap().as_slice() == "abc");
+      },
+      _ => fail!("Unexpected reaction!")
+    }
+  }
 }
