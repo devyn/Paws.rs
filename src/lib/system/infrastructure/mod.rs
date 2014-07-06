c9235d3d3b9aaa1e93ee512e1448cf130970dfb4
Devyn Cairns <devyn.cairns@gmail.com>
2014-07-06 00:16:00 -0700

    Add more things to system::infrastructure to make example 03 work

--- src/lib/system/infrastructure/mod.rs
+++ src/lib/system/infrastructure/mod.rs
@@ -25,19 +25,54 @@ pub fn make(machine: &Machine) -> ObjectRef {
     let mut add = NamespaceBuilder::new(machine, &mut *infrastructure);
 
     add.namespace(    "execution",               execution::make              );
+
+    add.call_pattern( "empty",                   empty, 0                     );
+
+    add.call_pattern( "get",                     get, 2                       );
+
     add.call_pattern( "affix",                   affix, 2                     );
     add.call_pattern( "unaffix",                 unaffix, 1                   );
+    add.call_pattern( "prefix",                  prefix, 2                    );
+    add.call_pattern( "unprefix",                unprefix, 2                  );
+
+    add.call_pattern( "length",                  length, 1                    );
+
+    add.call_pattern( "own",                     own, 2                       );
+    add.call_pattern( "disown",                  disown, 2                    );
   }
 
   ObjectRef::new(infrastructure)
+}
+
+pub fn empty(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+             -> Reaction {
+
+  React(caller, ObjectRef::new(box Thing::new()))
+}
+
+pub fn get(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+           -> Reaction {
+  match args {
+    [ref from, ref index] => {
+      let index = match unsignedish(index) {
+        Some(index) => index,
+        None        => return Yield
+      };
+
+      match from.lock().meta().members.get(index) {
+        Some(relationship) => React(caller, relationship.to().clone()),
+        None               => Yield
+      }
+    },
+    _ => fail!("wrong number of arguments")
+  }
 }
 
 pub fn affix(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
              -> Reaction {
   match args {
     [ref onto, ref what] => {
-      let mut onto = onto.lock();
-      onto.meta_mut().members.push(what.clone());
+      onto.lock().meta_mut().members.push(what.clone());
       Yield
     },
     _ => fail!("wrong number of arguments")
@@ -48,13 +83,94 @@ pub fn unaffix(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
                -> Reaction {
   match args {
     [ref from] => {
-      let mut from = from.lock();
-
-      match from.meta_mut().members.pop() {
+      match from.lock().meta_mut().members.pop() {
         Some(relationship) => React(caller, relationship.unwrap()),
         None               => Yield
       }
     },
     _ => fail!("wrong number of arguments")
   }
+}
+
+pub fn prefix(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+              -> Reaction {
+  match args {
+    [ref onto, ref what] => {
+      onto.lock().meta_mut().members.unshift(what.clone());
+      Yield
+    },
+    _ => fail!("wrong number of arguments")
+  }
+}
+
+pub fn unprefix(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+                -> Reaction {
+  match args {
+    [ref from] => {
+      match from.lock().meta_mut().members.shift() {
+        Some(relationship) => React(caller, relationship.unwrap()),
+        None               => Yield
+      }
+    },
+    _ => fail!("wrong number of arguments")
+  }
+}
+
+pub fn length(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+              -> Reaction {
+  match args {
+    [ref of] => {
+      let length = of.lock().meta().members.len() as int;
+
+      // We subtract 1 from the length because the noughty (#0) is not counted;
+      // this is the length of the "data"-members.
+      React(caller, machine.symbol((length - 1).to_str().as_slice()))
+    },
+    _ => fail!("wrong number of arguments")
+  }
+}
+
+pub fn own(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+           -> Reaction {
+  match args {
+    [ref on, ref index] => {
+      unsignedish(index).map(|index| {
+
+        if !on.lock().meta_mut().members.own(index) {
+          warn!("tried to own a nonexistent member #{} on {}",
+            index, on);
+        }
+
+      });
+    },
+    _ => fail!("wrong number of arguments")
+  }
+
+  Yield
+}
+
+pub fn disown(machine: &Machine, caller: ObjectRef, args: &[ObjectRef])
+              -> Reaction {
+  match args {
+    [ref on, ref index] => {
+      unsignedish(index).map(|index| {
+
+        if !on.lock().meta_mut().members.disown(index) {
+          warn!("tried to disown a nonexistent member #{} on {}",
+            index, on);
+        }
+
+      });
+    },
+    _ => fail!("wrong number of arguments")
+  }
+
+  Yield
+}
+
+// FIXME when ELLIOTTCABLE decides what he wants to do about numbers.
+fn unsignedish(symbol: &ObjectRef) -> Option<uint> {
+  symbol.symbol_ref().and_then(|string|
+    from_str::<uint>(string.as_slice())
+  )
 }
