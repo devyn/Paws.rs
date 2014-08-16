385f2facd199f8ef54c63908d82c54e1418642aa
Devyn Cairns <devyn.cairns@gmail.com>
2014-08-16 03:09:46 -0700

    Rust nightly: Timer now accepts Durations instead of u64

--- src/lib/util/mod.rs
+++ src/lib/util/mod.rs
@@ -1,6 +1,7 @@
 ///! Utility functions and structures.
 
 use std::io::timer::Timer;
+use std::time::duration::Duration;
 
 pub mod namespace;
 pub mod clone;
@@ -8,7 +9,7 @@ pub mod clone;
 /// Spawn the given block and fail if the timeout is reached before it
 /// completes.
 #[allow(dead_code)] // ??
-pub fn timeout(msecs: u64, block: proc(): Send) {
+pub fn timeout(msecs: i32, block: proc(): Send) {
   let (complete_tx, complete_rx) = channel::<()>();
 
   let mut timer = Timer::new().unwrap();
@@ -18,7 +19,7 @@ pub fn timeout(msecs: u64, block: proc(): Send) {
     complete_tx.send(());
   });
 
-  let timeout = timer.oneshot(msecs);
+  let timeout = timer.oneshot(Duration::milliseconds(msecs));
 
   select!(
     () = timeout.recv()     => fail!("timeout ({}ms) reached!", msecs),
